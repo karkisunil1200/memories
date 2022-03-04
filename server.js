bebd4024b8333f0efe2bcd3dd03880c672824f9a
sunilkarki <karkisunil1200@gmail.com>
2022-03-04 09:26:51 -0600

    Added mongo database

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Memories hompage is going live for the first time");
